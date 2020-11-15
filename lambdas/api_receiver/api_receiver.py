a550853daee100a5504d96e713f4aa02cf0836f6
Nizar Thinkpad Windows <nizaralmesry@hotmail.com>
2020-11-15 19:07:28 +0200

    UPD api_receiver to correctly handle float numbers

--- lambdas/api_receiver/api_receiver.py
+++ lambdas/api_receiver/api_receiver.py
@@ -3,14 +3,15 @@ import json
 import logging
 import os
 from boto3.dynamodb.conditions import Key
+from decimal import Decimal
 
 # set up nice logging in CloudWatch
 LOGGER = logging.getLogger()
 LOGGER.setLevel(logging.INFO)
 
 DYNAMO_DB = boto3.resource('dynamodb')
-CALL_TABLE = DYNAMO_DB.Table('infra-smartnumbers-call')
-OPERATOR_TABLE = DYNAMO_DB.Table('infra-smartnumbers-operator')
+CALLS_TABLE = DYNAMO_DB.Table('infra-smartnumbers-calls')
+OPERATORS_TABLE = DYNAMO_DB.Table('infra-smartnumbers-operators')
 
 
 def respond(err: ValueError, res: str = None) -> json:
@@ -38,23 +39,33 @@ def handler(event, context):
 
 
 def process_call_event(data_item):
+    attributes = data_item.get('attributes')
+    id: str = data_item['id']
+    date: str = attributes['date']
+    riskScore  = Decimal(str(attributes['riskScore']))
+    number: str = attributes.get('number', 'Withheld')
+    greenList: bool = attributes['greenList']
+    redList: bool = attributes['redList']
+
     item = {
-        'id': data_item['id'],
-        'date': data_item['attributes']['date'],
-        'riskScore': data_item['attributes']['riskScore'],
-        'number': data_item['attributes']['number'],
-        'greenList': data_item['attributes']['greenList'],
-        'redList': data_item['attributes']['redList']
+        'id': id,
+        'date': date,
+        'riskScore': riskScore,
+        'number': number,
+        'greenList': greenList,
+        'redList': redList
     }
 
     return item
 
 
 def porcess_operator_event(data_item):
+    attributes = data_item.get('attributes')
+
     item = {
         'id': data_item['id'],
-        'prefix': data_item['attributes']['prefix'],
-        'operator': data_item['attributes']['operator']
+        'prefix': attributes['prefix'],
+        'operator': attributes['operator']
     }
 
     return item
@@ -63,25 +74,26 @@ def porcess_operator_event(data_item):
 def event_receiver_POST(event):
     body = json.loads(event['body'])
 
-    for data_item in body:
+    for data_item in body['data']:
         item = {}
-        item_type = body['type']
-
-        if item_type == 'call':
-            item = process_call_event(data_item)
-        elif item_type == 'operator':
-            item = porcess_operator_event(data_item)
-        else:
-            return respond(ValueError('Unsupported method "{}"'.format(item_type)))
 
         try:
+            item_type = data_item['type']
+
             if item_type == 'call':
-                CALL_TABLE.put_item(
-                    item
+                item = process_call_event(data_item)
+            elif item_type == 'operator':
+                item = porcess_operator_event(data_item)
+            else:
+                return respond(ValueError('Unsupported method "{}"'.format(item_type)))
+
+            if item_type == 'call':
+                CALLS_TABLE.put_item(
+                    Item = item
                 )
             elif item_type == 'operator':
-                OPERATOR_TABLE.put_item(
-                    item
+                OPERATORS_TABLE.put_item(
+                    Item = item
                 )
 
         except Exception as e:
@@ -93,9 +105,9 @@ def event_receiver_POST(event):
                 'error': e
             }
 
-        LOGGER.info("ITEM: " + data_item)
+        LOGGER.info("ITEM received: " + json.dumps(data_item))
 
-        return {
-            'statusCode': '200',
-            'body': data_item
-        }
+    return {
+        'statusCode': '200',
+        'body': json.dumps(body)
+    }
