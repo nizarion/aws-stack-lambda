92363f59b547a20841303cfcd21852f30c818c1e
Nizar Thinkpad Windows <nizaralmesry@hotmail.com>
2020-11-16 12:07:02 +0200

    CLEANUP api_receiver

--- lambdas/api_receiver/api_receiver.py
+++ lambdas/api_receiver/api_receiver.py
@@ -1,14 +1,13 @@
 import boto3
 import json
 import logging
-import os
-from boto3.dynamodb.conditions import Key
 from decimal import Decimal
 
 # set up nice logging in CloudWatch
 LOGGER = logging.getLogger()
 LOGGER.setLevel(logging.INFO)
 
+#set up dynamodb
 DYNAMO_DB = boto3.resource('dynamodb')
 CALLS_TABLE = DYNAMO_DB.Table('infra-smartnumbers-calls')
 OPERATORS_TABLE = DYNAMO_DB.Table('infra-smartnumbers-operators')
@@ -36,12 +35,51 @@ def handler(event, context):
         return respond(ValueError('Unsupported method "{}"'.format(operation)))
 
 
+def event_receiver_POST(event):
+    body = json.loads(event['body'])
+
+    for data_item in body['data']:
+        item = {}
+
+        try:
+            item_type = data_item['type']
+
+            if item_type == 'call':
+                item = process_call_event(data_item)
+                CALLS_TABLE.put_item(
+                    Item=item
+                )
+            elif item_type == 'operator':
+                item = porcess_operator_event(data_item)
+                OPERATORS_TABLE.put_item(
+                    Item=item
+                )
+            else:
+                return respond(ValueError('Unsupported method "{}"'.format(item_type)))
+        
+        except Exception as e:
+            LOGGER.error("Error processing object. Event {}".format(
+                json.dumps(data_item)))
+            LOGGER.error(e)
+            return {
+                'statusCode': '400',
+                'error': e
+            }
+
+        LOGGER.info("ITEM received: " + json.dumps(data_item))
+
+    return {
+        'statusCode': '200',
+        'body': json.dumps(body)
+    }
+
+
 def process_call_event(data_item):
     attributes = data_item.get('attributes')
-    
+
     id: str = data_item['id']
     date: str = attributes['date']
-    riskScore  = Decimal(str(attributes['riskScore']))
+    riskScore = Decimal(str(attributes['riskScore']))
     number: str = attributes.get('number', 'Withheld')
     greenList: bool = attributes['greenList']
     redList: bool = attributes['redList']
@@ -68,45 +106,3 @@ def porcess_operator_event(data_item):
     }
 
     return item
-
-
-def event_receiver_POST(event):
-    body = json.loads(event['body'])
-
-    for data_item in body['data']:
-        item = {}
-
-        try:
-            item_type = data_item['type']
-
-            if item_type == 'call':
-                item = process_call_event(data_item)
-            elif item_type == 'operator':
-                item = porcess_operator_event(data_item)
-            else:
-                return respond(ValueError('Unsupported method "{}"'.format(item_type)))
-
-            if item_type == 'call':
-                CALLS_TABLE.put_item(
-                    Item = item
-                )
-            elif item_type == 'operator':
-                OPERATORS_TABLE.put_item(
-                    Item = item
-                )
-
-        except Exception as e:
-            LOGGER.error("Error processing object. Event {}".format(
-                json.dumps(data_item)))
-            LOGGER.error(e)
-            return {
-                'statusCode': '400',
-                'error': e
-            }
-
-        LOGGER.info("ITEM received: " + json.dumps(data_item))
-
-    return {
-        'statusCode': '200',
-        'body': json.dumps(body)
-    }
